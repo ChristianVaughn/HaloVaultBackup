cf664acd12806defef712f36caa172fd94277315
OatmealRasin <cvaughn55jp@gmail.com>
2019-03-20 23:52:09 -0700

    First draft of site
    First Draft of site

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 $( document ).ready(function() {
+console.log('START SCRIPT');
+ $.getJSON("https://raw.githubusercontent.com/cvaughn55/HaloVaultBackup/master/data.json?token=AEbY1FQQDsT2s0HL1CoOz143kI0XwAQfks5cnGY9wA%3D%3D", function(data) {    $.each(data, function(index, element) {
+	 console.log(element.MapName.toLowerCase().charAt(0));
 
-$.getJSON('./data.json', { get_param: 'value' }, function(data) {
-    $.each(data, function(index, element) {
-        const card = document.createElement('a');
+    const card = document.createElement('a');
     card.setAttribute('class', 'card');
     card.href = element.Dlink
 
     const mapImage = document.createElement('figure');
-    mapImage.class = element.BaseMap;
+    mapImage.setAttribute('class', element.BaseMap);
     
     const mapName = document.createElement('article');
     mapName.setAttribute('class', 'text');
-
+	
     const h2 = document.createElement('h2');
-    h2.textContent = element.title.MapName
+    h2.textContent = element.MapName
 
     if ('0123456789'.includes(element.MapName.charAt(0))) {
-        const app = document.getElementById('a');
+        var app = document.getElementById('aa');
     }
     else {
-        const app = document.getElementById(element.MapName.toLowerCase().charAt(0));
+        var app = document.getElementById(element.MapName.toLowerCase().charAt(0));
     }
+	 
 
     app.appendChild(card);
     card.appendChild(mapImage);
